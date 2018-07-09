e0950dd1a3cf119e7edaaa6d65f52e91fef8016c
Tal Aloni <tal.aloni.il@gmail.com>
2018-07-09 18:37:11 +0300

    Dynamic Disk Partitioner v1.4.7

--- DynamicDiskPartitioner/Properties/AssemblyInfo.cs
+++ DynamicDiskPartitioner/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tal Aloni")]
 [assembly: AssemblyProduct("Dynamic Disk Partitioner")]
-[assembly: AssemblyCopyright("Copyright © Tal Aloni 2012-2017")]
+[assembly: AssemblyCopyright("Copyright © Tal Aloni 2012-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.6.0")]
-[assembly: AssemblyFileVersion("1.4.6.0")]
+[assembly: AssemblyVersion("1.4.7.0")]
+[assembly: AssemblyFileVersion("1.4.7.0")]
