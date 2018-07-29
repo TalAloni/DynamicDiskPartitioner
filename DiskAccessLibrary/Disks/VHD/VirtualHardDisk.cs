68cb26714e66a1f5ef3011a1254bcd8aa4ae71f8
Tal Aloni <tal.aloni.il@gmail.com>
2018-07-29 18:45:46 +0300

    VHD: BlockAllocationTable: Improved implementation

--- DiskAccessLibrary/Disks/VHD/VirtualHardDisk.cs
+++ DiskAccessLibrary/Disks/VHD/VirtualHardDisk.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014-2016 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2018 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -113,9 +113,9 @@ namespace DiskAccessLibrary
                     int sectorsRemainingInBlock = sectorsInBlock - sectorOffsetInBlock;
                     int sectorsToRead = Math.Min(sectorCount - sectorOffset, sectorsRemainingInBlock);
 
-                    if (m_blockAllocationTable.IsBlockInUse(blockIndex))
+                    uint blockStartSector;
+                    if (m_blockAllocationTable.IsBlockInUse(blockIndex, out blockStartSector))
                     {
-                        uint blockStartSector = m_blockAllocationTable.Entries[blockIndex];
                         // each data block has a sector bitmap preceding the data, the bitmap is padded to a 512-byte sector boundary.
                         int blockBitmapSectorCount = (int)Math.Ceiling((double)sectorsInBlock / (this.BytesPerSector * 8));
                         // "All sectors within a block whose corresponding bits in the bitmap are zero must contain 512 bytes of zero on disk"
