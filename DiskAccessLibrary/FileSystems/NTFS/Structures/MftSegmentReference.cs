22c55e043eb9cd4ccf12a0aedd2e564f13e23cc0
Tal Aloni <tal.aloni.il@gmail.com>
2018-08-02 09:06:57 +0300

    NTFS: MftSegmentReference: Added constructor

--- DiskAccessLibrary/FileSystems/NTFS/Structures/MftSegmentReference.cs
+++ DiskAccessLibrary/FileSystems/NTFS/Structures/MftSegmentReference.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2018 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -6,7 +6,6 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Text;
 using Utilities;
 
 namespace DiskAccessLibrary.FileSystems.NTFS
@@ -17,6 +16,12 @@ namespace DiskAccessLibrary.FileSystems.NTFS
 
         public long SegmentNumber; // 6 bytes
         public ushort SequenceNumber;
+
+        public MftSegmentReference(long segmentNumber, ushort sequenceNumber)
+        {
+            SegmentNumber = segmentNumber;
+            SequenceNumber = sequenceNumber;
+        }
         
         public MftSegmentReference(byte[] buffer, int offset)
         {
