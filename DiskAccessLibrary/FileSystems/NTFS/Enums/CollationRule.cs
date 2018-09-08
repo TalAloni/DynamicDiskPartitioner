dce76b5b761a883e7581911a041bd2088b893712
Tal Aloni <tal.aloni.il@gmail.com>
2018-09-08 10:52:36 +0300

    NTFS: CollationRule: Improved documentation

--- DiskAccessLibrary/FileSystems/NTFS/Enums/CollationRule.cs
+++ DiskAccessLibrary/FileSystems/NTFS/Enums/CollationRule.cs
@@ -3,12 +3,12 @@ namespace DiskAccessLibrary.FileSystems.NTFS
 {
     public enum CollationRule : uint
     {
-        Binary = 0x00000000,
-        Filename = 0x00000001,
-        UnicodeString = 0x00000002,
+        Binary = 0x00000000,                // COLLATION_BINARY
+        Filename = 0x00000001,              // COLLATION_FILE_NAME
+        UnicodeString = 0x00000002,         // COLLATION_UNICODE_STRING
         UnsignedLong = 0x00000010,
         Sid = 0x00000011,
         SecurityHash = 0x00000012,
-        MultipleUnsignedLongs = 0x00000013
+        MultipleUnsignedLongs = 0x00000013,
     }
 }
