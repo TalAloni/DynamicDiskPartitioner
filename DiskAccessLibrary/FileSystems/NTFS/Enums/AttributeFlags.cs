732fea0e355866eca82ce3e6b2cd1a421754fc33
Tal Aloni <tal.aloni.il@gmail.com>
2018-09-21 22:48:06 +0300

    NTFS: AttributeFlags: Added CompressedUsingLZNT1

--- DiskAccessLibrary/FileSystems/NTFS/Enums/AttributeFlags.cs
+++ DiskAccessLibrary/FileSystems/NTFS/Enums/AttributeFlags.cs
@@ -8,8 +8,9 @@ namespace DiskAccessLibrary.FileSystems.NTFS
     [Flags]
     public enum AttributeFlags : ushort
     {
-        Encrypted = 0x4000,       // ATTRIBUTE_FLAG_ENCRYPTED
-        Sparse = 0x8000,          // ATTRIBUTE_FLAG_SPARSE
-        CompressionMask = 0x00FF, // ATTRIBUTE_FLAG_COMPRESSION_MASK
+        CompressedUsingLZNT1 = 0x0001, // COMPRESSION_FORMAT_LZNT1 - 1
+        CompressionMask = 0x00FF,      // ATTRIBUTE_FLAG_COMPRESSION_MASK
+        Encrypted = 0x4000,            // ATTRIBUTE_FLAG_ENCRYPTED
+        Sparse = 0x8000,               // ATTRIBUTE_FLAG_SPARSE
     }
 }
