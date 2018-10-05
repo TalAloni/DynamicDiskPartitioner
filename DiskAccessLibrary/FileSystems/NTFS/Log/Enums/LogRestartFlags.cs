b410a9e9ef7e8dab9cbfd68b056b8d6740adbdc3
Tal Aloni <tal.aloni.il@gmail.com>
2018-10-05 17:19:55 +0300

    NTFS: LogRestartFlags: Added LogFileIsClean

--- DiskAccessLibrary/FileSystems/NTFS/Log/Enums/LogRestartFlags.cs
+++ DiskAccessLibrary/FileSystems/NTFS/Log/Enums/LogRestartFlags.cs
@@ -6,5 +6,6 @@ namespace DiskAccessLibrary.FileSystems.NTFS
     public enum LogRestartFlags : ushort
     {
         RestartSinglePageIO = 0x0001, // RESTART_SINGLE_PAGE_IO
+        LogFileIsClean = 0x0002,      // NTFS v3.1
     }
 }
