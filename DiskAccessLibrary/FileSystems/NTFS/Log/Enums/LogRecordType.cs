26dc7dc6474d2994850f165f19bc94f65cfbce43
Tal Aloni <tal.aloni.il@gmail.com>
2018-10-05 11:56:49 +0300

    NTFS: LogRecordType enum: Added ClientRestart

--- DiskAccessLibrary/FileSystems/NTFS/Log/Enums/LogRecordType.cs
+++ DiskAccessLibrary/FileSystems/NTFS/Log/Enums/LogRecordType.cs
@@ -3,6 +3,7 @@ namespace DiskAccessLibrary.FileSystems.NTFS
 {
     public enum LogRecordType : uint
     {
-        ClientRecord = 1, // LfsClientRecord
+        ClientRecord = 1,  // LfsClientRecord
+        ClientRestart = 2, // LfsClientRestart
     }
 }
