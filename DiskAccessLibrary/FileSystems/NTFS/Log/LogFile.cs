8494f2a900501c18287ba6fa924024bb47c6db54
Tal Aloni <tal.aloni.il@gmail.com>
2018-11-06 08:39:34 +0200

    NTFS: LogFile: Removed comment

--- DiskAccessLibrary/FileSystems/NTFS/Log/LogFile.cs
+++ DiskAccessLibrary/FileSystems/NTFS/Log/LogFile.cs
@@ -457,7 +457,6 @@ namespace DiskAccessLibrary.FileSystems.NTFS
             }
         }
 
-        // Placeholder method that will implement caching in the future
         private LfsRecordPage ReadPage(ulong pageOffset)
         {
             if (m_tailPage != null && pageOffset == m_tailPageOffsetInFile)
