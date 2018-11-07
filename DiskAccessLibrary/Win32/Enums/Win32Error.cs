c27d9f9eeadb6117e1d50f90eef4d33d03212e5c
Tal Aloni <tal.aloni.il@gmail.com>
2018-11-07 15:35:53 +0200

    Added InvalidPathException implementation

--- DiskAccessLibrary/Win32/Enums/Win32Error.cs
+++ DiskAccessLibrary/Win32/Enums/Win32Error.cs
@@ -19,6 +19,7 @@ namespace DiskAccessLibrary
         ERROR_DISK_FULL = 0x0070,
         ERROR_INSUFFICIENT_BUFFER = 0x007A,
         ERROR_DIR_NOT_EMPTY = 0x0091,
+        ERROR_BAD_PATHNAME = 0x00A1,
         ERROR_ALREADY_EXISTS = 0x00B7,
         ERROR_MORE_DATA = 0x00EA, // buffer was not long enough
         ERROR_NO_MORE_ITEMS = 0x0103,
