2fc35ae16c0103b736baeeac4e0aa59cbc42b600
Tal Aloni <tal.aloni.il@gmail.com>
2023-10-07 03:04:54 +0300

    VMDK: VirtualMachineDiskExtentEntry: Improved readability

--- DiskAccessLibrary/Disks/VMDK/VirtualMachineDiskExtentEntry.cs
+++ DiskAccessLibrary/Disks/VMDK/VirtualMachineDiskExtentEntry.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2023 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -17,7 +17,7 @@ namespace DiskAccessLibrary.VMDK
         public long SizeInSectors;
         public ExtentType ExtentType;
         public string FileName;
-        public Nullable<long> Offset;
+        public long? Offset;
 
         public string GetEntryLine()
         {
